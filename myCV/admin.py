222c6bb8c207a619e807b6623f0892e4b3ffcdf4
Erkan KIYAK <erkank@hotmail.com>
2024-03-19 18:32:48 +0300

    general setting modeline General Setting ekleme

--- myCV/admin.py
+++ myCV/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from myCV.models import *
 
-# Register your models here.
+
+@admin.register(GeneralSetting)
+class GeneralSettingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'parameter', 'created_date', 'updated_date']
+    search_fields = ['name', 'description', 'parameter']
+    list_editable = ['description', 'parameter']
+    ordering = ["name"]
+
+    class Meta:
+        model = GeneralSetting
--- myCV/models.py
+++ myCV/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class GeneralSetting(models.Model):
+    name = models.CharField(max_length=255, default='', blank=True, verbose_name="name", help_text="This is variable of te settings")
+    description = models.CharField(max_length=255, default='', blank=True, verbose_name="description", help_text="")
+    parameter = models.CharField(max_length=255, default='', blank=True, verbose_name="parameter", help_text="")
+    created_date = models.DateTimeField(auto_now_add=True, blank=True, verbose_name="created_date", help_text="")
+    updated_date = models.DateTimeField(auto_now=True, blank=True, verbose_name="updated_date", help_text="")
+
+
+def __str__(self):
+    return f'General Setting: {self.name}'
+
+
+class Meta:
+    verbose_name = 'General Setting'
+    verbose_name_plural = 'General Settings'
+    ordering = ('name')
