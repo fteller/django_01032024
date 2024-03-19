cde62e1735509bec40e7f321e638f1a9c664468e
Erkan KIYAK <erkank@hotmail.com>
2024-03-19 22:05:15 +0300

    general setting modeli Frontend Uygulaması2

--- myCV/models.py
+++ myCV/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class GeneralSetting(models.Model):
-    name = models.CharField(max_length=255, default='', blank=True, verbose_name="name", help_text="This is variable of te settings")
+    name = models.CharField(max_length=255, default='', blank=True, verbose_name="name", help_text="This is variable of the settings")
     description = models.CharField(max_length=255, default='', blank=True, verbose_name="description", help_text="")
     parameter = models.CharField(max_length=255, default='', blank=True, verbose_name="parameter", help_text="")
     created_date = models.DateTimeField(auto_now_add=True, blank=True, verbose_name="created_date", help_text="")
@@ -18,3 +18,11 @@ class Meta:
     verbose_name = 'General Setting'
     verbose_name_plural = 'General Settings'
     ordering = ('name')
+
+
+class ImageSetting(models.Model):
+    name = models.CharField(max_length=255, default='', blank=True, verbose_name="name", help_text="This is variable of the settings")
+    description = models.CharField(max_length=255, default='', blank=True, verbose_name="description", help_text="")
+    file = models.ImageField(default='', blank=True, verbose_name="Image", help_text="", upload_to='images/')
+    created_date = models.DateTimeField(auto_now_add=True, blank=True, verbose_name="created_date", help_text="")
+    updated_date = models.DateTimeField(auto_now=True, blank=True, verbose_name="updated_date", help_text="")
