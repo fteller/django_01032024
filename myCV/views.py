79e3cdb0535884a813a889e085f08d5268c1988d
Erkan KIYAK <erkank@hotmail.com>
2024-03-19 23:30:33 +0300

    Image setting modeli fronthand uygulaması

--- myCV/views.py
+++ myCV/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import GeneralSetting
+from .models import GeneralSetting, ImageSetting
 
 
 # Create your views here.
@@ -16,6 +16,11 @@ def index(request):
     about_myself_welcome = GeneralSetting.objects.get(name='about_myself_welcome').parameter
     about_myself_footer = GeneralSetting.objects.get(name='about_myself_footer').parameter
 
+    #images
+    header_logo = ImageSetting.objects.get(name='header_logo').file
+    home_banner_image = ImageSetting.objects.get(name='home_banner_image').file
+    site_favicon = ImageSetting.objects.get(name='site_favicon').file
+
     context = {
         'site_title': site_title,
         'site_keywords': site_keywords,
@@ -26,5 +31,8 @@ def index(request):
         'home_banner_description': home_banner_description,
         'about_myself_welcome': about_myself_welcome,
         'about_myself_footer': about_myself_footer,
+        'header_logo': header_logo,
+        'home_banner_image': home_banner_image,
+        'site_favicon': site_favicon,
     }
     return render(request, 'index.html', context=context)
