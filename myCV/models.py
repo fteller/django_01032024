240b0660398c52d9c0452a61f80df2fbab0b21d1
Erkan KIYAK <erkank@hotmail.com>
2024-03-20 19:45:49 +0300

    Abstract model oluşturma

--- myCV/models.py
+++ myCV/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
+class AbstractModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True, blank=True, verbose_name="created_date", help_text="")
+    updated_date = models.DateTimeField(auto_now=True, blank=True, verbose_name="updated_date", help_text="")
+
+    class Meta:
+        abstract = True
 
 # Create your models here.
-class GeneralSetting(models.Model):
+class GeneralSetting(AbstractModel):
     name = models.CharField(max_length=255, default='', blank=True, verbose_name="name", help_text="This is variable of the settings")
     description = models.CharField(max_length=255, default='', blank=True, verbose_name="description", help_text="")
     parameter = models.CharField(max_length=255, default='', blank=True, verbose_name="parameter", help_text="")
@@ -20,12 +26,10 @@ class GeneralSetting(models.Model):
         ordering = ('name',)
 
 
-class ImageSetting(models.Model):
+class ImageSetting(AbstractModel):
     name = models.CharField(max_length=255, default='', blank=True, verbose_name="name", help_text="This is variable of the settings")
     description = models.CharField(max_length=255, default='', blank=True, verbose_name="description", help_text="")
     file = models.ImageField(default='', blank=True, verbose_name="Image", help_text="", upload_to='images/')
-    created_date = models.DateTimeField(auto_now_add=True, blank=True, verbose_name="created_date", help_text="")
-    updated_date = models.DateTimeField(auto_now=True, blank=True, verbose_name="updated_date", help_text="")
 
     def __str__(self):
         return f'Image Setting: {self.name}'
