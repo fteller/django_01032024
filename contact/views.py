db89c8c5b9ab85f06f21a88c1e014cea1a1501c5
Erkan KIYAK <erkank@hotmail.com>
2024-03-23 14:18:00 +0300

    Django Forms

--- contact/views.py
+++ contact/views.py
@@ -1,20 +1,26 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from contact.models import Message
+from contact.forms import ContactForm
 
 
 # Create your views here.
 def contact_form(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        subject = request.POST.get('subject')
-        message = request.POST.get('message')
+        contact_form = ContactForm(request.POST or None)
+        if contact_form.is_valid():
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            subject = request.POST.get('subject')
+            message = request.POST.get('message')
 
-        Message.objects.create(name=name, email=email, subject=subject, message=message)
+            Message.objects.create(name=name, email=email, subject=subject, message=message)
 
-        success = True
-        message_sent = 'Contact form sent successfully'
+            success = True
+            message_sent = 'Contact form sent successfully'
+        else:
+            success = False
+            message_sent = 'Contact form is not valid!'
     else:
         success = False
         message_sent = 'Request method is not valid!'
@@ -27,4 +33,8 @@ def contact_form(request):
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    contact_form = ContactForm()
+    context = {
+        'contact_form': contact_form
+    }
+    return render(request, 'contact.html', context=context)
